01776d3b87ea5f0e7d35cff7ab4c9baa1acf8a9f
Matheus Marmo <matheusmarmo55@gmail.com>
2019-05-22 13:18:36 -0300

    teste exchange service

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,10 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  require 'webmock/rspec'
+  WebMock.disable_net_connect!(allow_localhost: true)
+  require File.expand_path("../../config/environment", __FILE__)
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
